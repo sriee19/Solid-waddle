cc9dc0b529cc3f1567aee092f0549318d157dfc3
SrisanjanaA <88726874+sriee19@users.noreply.github.com>
2023-04-23 19:49:55 +0530

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,36 @@
+export function getAboutUsLink(language) {
+  switch (language.toLowerCase()) {
+    case englishCode.toLowerCase():
+      return "/about-us";
+    case spanishCode.toLowerCase():
+      return "/acerca-de";
+  }
+  return "";
+}
+export function getAboutUsLink(language) {
+  switch (language.toLowerCase()) {
+    case englishCode.toLowerCase():
+      return "/about-us";
+    case spanishCode.toLowerCase():
+      return "/acerca-de";
+  }
+  return "";
+}
+export function getAboutUsLink(language) {
+  switch (language.toLowerCase()) {
+    case englishCode.toLowerCase():
+      return "/about-us";
+    case spanishCode.toLowerCase():
+      return "/acerca-de";
+  }
+  return "";
+}
+export function getAboutUsLink(language) {
+  switch (language.toLowerCase()) {
+    case englishCode.toLowerCase():
+      return "/about-us";
+    case spanishCode.toLowerCase():
+      return "/acerca-de";
+  }
+  return "";
+}
